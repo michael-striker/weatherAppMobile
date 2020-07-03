9243846b322cd57cfcea9f477d310855fe22771e
Михаил Шмыков <m7shmykov@gmail.com>
2020-07-03 22:41:36 +0300

    add select search

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import  React, {useState, useEffect} from 'react';
-import { Text, View, ScrollView, StyleSheet, Image, ActivityIndicator} from 'react-native';
+import { Text, View, ScrollView, StyleSheet, Image, ActivityIndicator, TextInput, Button} from 'react-native';
 import Constants from 'expo-constants';
 import { LinearGradient } from 'expo-linear-gradient';
 
@@ -7,20 +7,119 @@ import { LinearGradient } from 'expo-linear-gradient';
 export default function App() {
   
   const [weatherData, setWeatherData] = useState(null);
+  const [response, setResponse] = useState('');
+  console.log(response)
+
   
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(function showPosition(position) {
         const lat = position.coords.latitude;
         const lon = position.coords.longitude;
-        const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=7c8e042c09a447200763fa7fff3a0e64&units=metric&lang=ru`;
-
+        const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=7c8e042c09a447200763fa7fff3a0e64&units=metric&lang=ru`; 
+    // Not using https://www.metaweather.com/api/ because access to fetch has been blocked by CORS policy. Besides 'openweathermap' has better geolocation options and more locations to find.
+  
     fetch(url)
-    .then(response => response.json())
+    .then(res => res.json())
     .then(data => {
-        setWeatherData(data);
+      
+      
+    
+        setWeatherData(data)
+    
+      
     })
-})}, []);
+  })}, []); // get current weather
+  
+  const returnToStart = () => {
+    setWeatherData(
+      () => {
+        navigator.geolocation.getCurrentPosition(function showPosition(position) {
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
+            const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=7c8e042c09a447200763fa7fff3a0e64&units=metric&lang=ru`;
+    
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setWeatherData(data))
+    })}
+    )
+  };// get back to start
+  
+  const weatherCheck = (submit) => {
+    const location = submit.nativeEvent.text
+    console.log(submit.nativeEvent.text)
+    setTimeout(() => {
+      const url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=7c8e042c09a447200763fa7fff3a0e64&units=metric&lang=ru`;
+    
+      fetch(url)
+      .then(res => res.json())
+      .then(data => setWeatherData(data))
+    }, 1000);
 
+      
+         // Set timeout for better UI
+  };// get select weather
+  
+  if (!weatherData ) {
+    return <ActivityIndicator size='large' color='#0000ff' />
+  }; // Render when we haven't any data from server
+  if (weatherData.cod === '404') {
+    return (
+      <View style={styles.root}>
+      <LinearGradient
+            colors={['rgba(2,0,36,1)','rgba(83,83,110,0)', 'rgba(0,255,236,0.35)', 'transparent']}
+            style={{
+              position: 'absolute',
+              left: 0,
+              right: 0,
+              top: 0,
+              height: 1200,
+            }}
+          />
+      <ScrollView>
+      <View style={styles.container}>
+               <Text style={styles.header}>
+                    Населённый пункт не найдет!
+               </Text>
+               <Text style={styles.paragraph}>
+               Возможна ошибка при вводе.
+               </Text>
+               <Text style={styles.paragraph}>
+               Вернитесь к началу и повторите попытку.
+               </Text>
+                   <Button title='Вернуться к началу' onPress={returnToStart} />
+      </View>
+      </ScrollView>
+      </View>                   
+    )
+  }; // Render when we get cod 404
+  if (weatherData.cod === '400') {
+    return (
+      <View style={styles.root}>
+      <LinearGradient
+            colors={['rgba(2,0,36,1)','rgba(83,83,110,0)', 'rgba(0,255,236,0.35)', 'transparent']}
+            style={{
+              position: 'absolute',
+              left: 0,
+              right: 0,
+              top: 0,
+              height: 1200,
+            }}
+          />
+      <ScrollView>
+      <View style={styles.container}>
+               <Text style={styles.header}>
+                    Вы ничего не ввели!
+               </Text>
+               <Text style={styles.paragraph}>
+               Вернитесь к началу и введите населённый пункт для поиска.
+               </Text>
+                   <Button title='Вернуться к началу' onPress={returnToStart} />
+      </View>
+      </ScrollView>
+      </View>                   
+    )
+  }; // Render when we get cod 400
 
   return (
     <View style={styles.root}>
@@ -34,17 +133,22 @@ export default function App() {
               height: 1200,
             }}
           />
-    {!weatherData ? <ActivityIndicator size='large' color='#0000ff' /> : (
       <ScrollView>
       <View style={styles.container}>
-            <Text style={styles.header}> 
-                {weatherData.name}
-                <Image 
-                style={styles.tinyLogo}
-                source={{
-                uri: `http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`,
-                }} />
-            </Text>
+            <TextInput style={styles.header}
+            defaultValue={weatherData.name} 
+            onChangeText={text => setResponse(text)}
+            onSubmitEditing={(submit) => weatherCheck(submit)}
+            returnKeyType={"search"}
+            /> 
+                <Text style={styles.paragraph}>
+                    Сейчас {weatherData.weather[0].description}
+                    <Image 
+                    style={styles.tinyLogo}
+                    source={{
+                    uri: `http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`,
+                    }} />
+                </Text>
             <Text style={styles.paragraph}>
                 Текущая температура {Math.round(weatherData.main.temp)} C° 
             </Text>
@@ -56,7 +160,6 @@ export default function App() {
             </Text>
         </View>
         </ScrollView>
-     )}
     </View>
   );
 }
@@ -82,6 +185,8 @@ const styles = StyleSheet.create({
   },
 
   header: {
+    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+    borderRadius: 7,
     margin: 20,
     fontSize: 24,
     fontWeight: 'bold',
